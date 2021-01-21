622353bbf44df8edae057d06b1dd3eb3588def70
Jason Michaels <jasonmichaelwhite@gmail.com>
2021-01-20 23:49:57 -0500

    ADDED: new fonts and header changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 
 import styles from './Header.module.css';
 
-const Header = () => (
-  <h1 className={styles.header}>
-    susmongus
-    {/* <img src="./img/banner.png" alt="banner" className={styles.bannerFinger} /> */}
-  </h1>
-);
+const Header = () => <h1 className={styles.header}>susmongus</h1>;
 
 export default Header;

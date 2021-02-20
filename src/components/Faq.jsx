9cc288ac055b8cf3df3dba160345ef3c4445d00b
Jason Michaels <jasonmichaelwhite@gmail.com>
2021-02-19 21:48:46 -0500

    JIC

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,37 +1,42 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+
 // eslint-disable-next-line import/no-cycle
 import Container from '../containers/Container';
 import styles from './Faq.module.css';
 
 const Faq = () => {
+  const handleLink = useCallback((e) => {
+    e.preventDefault();
+    require('electron').shell.openExternal(e.target.href);
+  }, []);
+
   return (
     <Container showHeader={false}>
+      <h1 className={styles.header}>You must install the Sus Tracker skill</h1>
       <ul className={styles.list}>
         <li className={styles.listItem}>
-          You must install the Sus Tracker Alexa&nbsp;&copy; skill
-        </li>
-        <li className={styles.listItem}>
-          Ask, <em>Alexa, get my code from Sus Tracker</em>
+          With the skill, ask, <em>Alexa, get my code from Sus Tracker</em>
         </li>
         <li className={styles.listItem}>
           Alexa will repeat back a six-digit code to enter to login to this app
         </li>
         <li className={styles.listItem}>
-          Next, you can add suspicion by saying,{' '}
-          <em>Alexa, add [color] using Sus Tracker</em>
+          Next, you can add suspicion by saying,&nbsp;
+          <em>Alexa, add [color] with Sus Tracker</em>
         </li>
         <li className={styles.listItem}>
-          Or, to remove sus from a color,{' '}
-          <em>Alexa, remove [color] using Sus Tracker</em>
+          Or, to remove sus from a color,&nbsp;ask,&nbsp;
+          <em>Alexa, remove [color] with Sus Tracker</em>
         </li>
         <li className={styles.listItem}>
-          And you can clear suspicion for all colors by saying,{' '}
-          <em>Alexa, clear all sus using Sus Tracker</em>
+          You can clear suspicion for all suspects by saying,&nbsp;
+          <em>Alexa, clear all sus with Sus Tracker</em>
         </li>
         <li className={styles.listItem}>
           For more information, visit&nbsp;
           <a
             className={styles.link}
+            onClick={handleLink}
             href="https://jogo.gg/privacy-policy-for-alexa-skills/"
           >
             Jogo&nbsp;
